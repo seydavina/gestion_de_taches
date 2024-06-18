477c591e26a9c6478fc14e3e3af2638c6d94bc46
Limssly <seydinalimamoulayeyade@gmail.com>
2024-06-18 15:42:08 +0000

    Integration des styles et des couleurs

---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color grey1 = Color(0xFF333333);
+  static const Color grey2 = Color(0xFF4F4F4F);
+  static const Color blue = Color(0xFF56CCF2);
+  static const Color green = Color(0xFF27AE60);
+  static const Color red = Color(0xFFEB5757);
+}
---
+++ lib/constants/style_text.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const TextStyle title1 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle title2 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle title3 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle title4 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
+}
